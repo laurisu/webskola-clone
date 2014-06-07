ac2fa0d29c9bdb48f14fed8d2096b5f659599644
armanc <armands.baris@gmail.com>
2014-06-07 11:01:32 +0300

    update config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../vendor/j4mie/idiorm/idiorm.php';
-require_once '../vendor/j4mie/paris/paris.php';
-
-ORM::configure('mysql:host=localhost;dbname=webskola-clone');
-ORM::configure('username', 'root');
-ORM::configure('password', 'password');
