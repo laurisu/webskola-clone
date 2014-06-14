65751b711d4ad3337179b2a888d45a8af8387bd8
laurisu <lauris.ulpe@gmail.com>
2014-06-14 16:59:44 +0300

    UPDATE: Admin panel and admin blog edit

--- controller/BlogController.php
+++ controller/BlogController.php
@@ -4,7 +4,7 @@ require '../model/BlogModel.php';
 
 class BlogControler {
 
-//    Katram routam savs action
+//    Katram routam savs Action
 
     public function listAction() {
 
@@ -25,18 +25,40 @@ class BlogControler {
         return $blogs;
     }
 
+    /**
+     * Object insert in database
+     * @param type $data = masīvs ar datiem priekš objekta
+     * @return boolean
+     */
     public function adminAddAction($data) {
-        $blog = $this->getFactory()->create();
-        $blog->slug = $data["slug"];
-        $blog->author = $data["author"];
+        $blog = $this->getFactory()->create(); // Šeit uztaisam jaunu objegtu
+        $blog->slug = $data["slug"];  // Aipildam objektu ar datiem 
+        $blog->author = $data["author"]; // objekta īpašība ir lauki datubāzē
         $blog->title = $data["blog-title"];
         $blog->description = $data["description"];
         $blog->content = $data["content"];
         
-        return $blog->save();
+        return $blog->save(); // saglabājam objektu iekš datubāzes
                 
 //        var_dump($data);
 //        exit;
+    }
+    
+    public function adminEditAction($id) {
+        $blogs = $this->getFactory()->find_one($id);
+        
+        return $blogs;
+    }
+    
+        public function adminUpdateAction($id, $data) {
+        $blog = $this->getFactory()->find_one($id); // Šeit sameklējam objegtu
+        $blog->slug = $data["slug"];  // Aizpildam objektu ar datiem 
+        $blog->author = $data["author"]; // objekta īpašība ir lauki datubāzē
+        $blog->title = $data["blog-title"];
+        $blog->description = $data["description"];
+        $blog->content = $data["content"];
+        
+        return $blog->save(); // saglabājam objektu iekš datubāzes
     }
 
     protected function getFactory() {
